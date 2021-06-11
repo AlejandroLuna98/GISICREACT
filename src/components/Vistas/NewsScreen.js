88d2330c0d2e9306942c45827dd6ca7aa8e0cd6d
jkguti <jagutierrez25@ucatolica.edu.co>
2021-06-10 19:25:25 -0500

    Actualización noticias

--- src/components/Vistas/NewsScreen.js
+++ src/components/Vistas/NewsScreen.js
@@ -20,44 +20,52 @@ export const NewsScreen = () => {
       <div>
         <Header />
         <Navbar />
-        <div className="container-md my-2">
-          <nav className=" nav navbar navbar-expand-lg navbar-light bg-light  ">
-            <div className="container-fluid d-flex justify-content-end">
-              <div className="navbar-collapse mx-3 text-center">
-                <div>
-                  <p>
-                    <h2 className="text-center pb-3">Noticia Nueva:</h2>
-                  </p>
-                  <p className=" mb-5" style={{textAlign: 'justify'}}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-                    viverra nibh quis ligula semper tempor. Nulla quis turpis quis
-                    diam sodales iaculis. Duis id luctus mi. Praesent sollicitudin
-                    pretium elit quis bibendum. Nullam ut nunc ipsum. Pellentesque non
-                    pharetra est. Quisque eu eros vel lorem tristique sodales. Aenean
-                    velit sapien, tempus et quam in, ultrices sollicitudin tellus.
-                    Vivamus vehicula porttitor lacus suscipit fringilla. Maecenas
-                    lacinia orci ac felis dictum mattis. Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Praesent viverra nibh quis ligula
-                    semper tempor. Nulla quis turpis quis diam sodales iaculis. Duis
-                    id luctus mi. Praesent sollicitudin pretium elit quis bibendum.
-                    Nullam ut nunc ipsum. Pellentesque non pharetra est. Quisque eu
-                    eros vel lorem tristique sodales. Aenean velit sapien, tempus et
-                    quam in, ultrices sollicitudin tellus. Vivamus vehicula porttitor
-                    lacus suscipit fringilla. Maecenas lacinia orci ac felis dictum
-                    mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Praesent viverra nibh quis ligula semper tempor. Nulla quis turpis
-                    quis diam sodales iaculis. Duis id luctus mi. 
-                  </p>
-                  <p style={{float: 'right'}}>
-                    <button
-                      className="btn btn-dark  justify-content-center py-2 mb-3 "
-                      onClick={handleReturn}
-                    >
-                      Atras
-                    </button>
-                  </p>
-                </div>
-              </div>
+        <div className="container p-0">
+          <nav className=" nav navbar-expand-lg">
+            <div className="col-lg-8-8 bg-light mx-7">
+              <p>
+                <h2 className="pb-3">Noticia Nueva:</h2>
+              </p>
+              <p className=" mb-5 text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+                viverra nibh quis ligula semper tempor. Nulla quis turpis quis
+                diam sodales iaculis. Duis id luctus mi. Praesent sollicitudin
+                pretium elit quis bibendum. Nullam ut nunc ipsum. Pellentesque non
+                pharetra est. Quisque eu eros vel lorem tristique sodales. Aenean
+                velit sapien, tempus et quam in, ultrices sollicitudin tellus.
+                Vivamus vehicula porttitor lacus suscipit fringilla. Maecenas
+                lacinia orci ac felis dictum mattis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Praesent viverra nibh quis ligula
+                semper tempor. Nulla quis turpis quis diam sodales iaculis. Duis
+                id luctus mi. Praesent sollicitudin pretium elit quis bibendum.
+                Nullam ut nunc ipsum. Pellentesque non pharetra est. Quisque eu
+                eros vel lorem tristique sodales. Aenean velit sapien, tempus et
+                quam in, ultrices sollicitudin tellus. Vivamus vehicula porttitor
+                lacus suscipit fringilla. Maecenas lacinia orci ac felis dictum
+                mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent viverra nibh quis ligula semper tempor. Nulla quis turpis
+                quis diam sodales iaculis. Duis id luctus mi. 
+              </p>
+              <p>
+                <button
+                  className="btn btn-dark  justify-content-center py-2 mb-3 "
+                  onClick={handleReturn}
+                >
+                  Atras
+                </button>
+              </p>
+            </div>
+            <div className="col-lg-1-1"></div>
+            <div className="col-lg-3-3 mx-6 bg-light">
+              <p>
+                <span className="bold">Autor: </span>Jessica gutierrez
+              </p>
+              <p>
+                <span className="bold">Fecha de publicación: </span>03/05/2021
+              </p>
+              <p>
+                <span className="bold">Twitter: </span><a href="https://twitter.com/JkGutiM" target="_blank">@JkGutiM</a>
+              </p>
             </div>
           </nav>
         </div>
